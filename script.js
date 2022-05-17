0937ce8abdcad27cf619b8ecacfbd56a1e3c53f5
Rikstone <tarikbouari@gmail.com>
2022-05-17 15:20:04 +0000

    youtube project

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-  /* Set the width of the side navigation to 250px */
-  function openNav(){
-    document.getElementById("navslide").style.width = "260px";
-    document.getElementById("navslide").style.transition = ".5s";
+/* Set the width of the side navigation to 250px */
+function openNav(){
+  document.getElementById("navslide").style.width = "260px";
+  document.getElementById("navslide").style.transition = ".5s";
 }
 
- /* Set the width of the side navigation to 0 */
- 
- function closeNav(){
-     
-     document.getElementById("navslide").style.width ="0px";
-     document.getElementById("navslide").style.transition = " .5s";
- }
- 
- 
+/* Set the width of the side navigation to 0 */
+
+function closeNav(){
+   
+   document.getElementById("navslide").style.width ="0px";
+   document.getElementById("navslide").style.transition = " .5s";
+}
+
