9a45a447002915a8529cf5fd51bc6fbd40d70577
Rikstone <tarikbouari@gmail.com>
2022-05-24 03:37:24 +0000

    added a theme functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+// browser would get acess to the javascript code when th page finish loading
+document.addEventListener('DOMcontentloaded', () =>{
+
+
 /* Set the width of the side navigation to 250px */
 function openNav(){
   document.getElementById("navslide").style.width = "260px";
@@ -9,6 +14,18 @@ function openNav(){
 function closeNav(){
    
    document.getElementById("navslide").style.width ="0px";
-   document.getElementById("navslide").style.transition = " .5s";
+   document.getElementById("navslide").style.transition = ".5s";
 }
 
+
+const pressMoon = document.querySelector("svg");
+const page = document.querySelector("body")
+
+document.addEventListener('click', () => {
+  pressMoon.classList.toggle('moon');
+  
+
+});
+
+
+});
